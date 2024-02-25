b312ced29204e8d6a0ea2af56282173309dd9774
ayush <ayuyshsharma1998@gmail.com>
2024-02-25 16:46:23 +0530

    -sign up completed i.e frontend connected to backend

--- frontend/chatapp/src/App.jsx
+++ frontend/chatapp/src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Home from "./pages/home/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
+      </Routes>
+      <Toaster></Toaster>
     </div>
   );
 }
