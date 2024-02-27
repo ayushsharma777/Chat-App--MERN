f3020f80370b0d28ca16c0584721ca56147387b4
ayush <ayuyshsharma1998@gmail.com>
2024-02-28 00:46:26 +0530

    completed

--- frontend/chatapp/src/components/messages/Messages.jsx
+++ frontend/chatapp/src/components/messages/Messages.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useRef } from "react";
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
+  useListenMessages();
 
 //for scrolling the chat till the end
 const lastMessageRef = useRef();
